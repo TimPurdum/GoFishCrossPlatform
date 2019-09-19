dc2fedc118bd9b0fb6be2deefb0612a940c19048
Tim Purdum <tpurdum@idnetworks.com>
2019-09-18 23:13:37 -0500

    Working Blazor and Console apps

---
+++ GoFish.Blazor/Pages/GamePage.razor.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace GoFish.Blazor.Pages
+{
+    public class GamePage : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
